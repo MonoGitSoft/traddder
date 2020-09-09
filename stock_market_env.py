c3d36b3d623a14c9f3fee84e9062fb797f7547d5
MonoGitSoft <monoxbox360@gmail.com>
2020-09-09 08:02:56 +0200

    Update stock_market_env.py

--- stock_market_env.py
+++ stock_market_env.py
@@ -1,14 +1,15 @@
-import gym
 import numpy as np
 import pandas as pd
 import math
 from gym import spaces
-from stable_baselines.common.policies import  MlpLstmPolicy
+import gym
+from stable_baselines.common.policies import MlpLstmPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
 
+
 class StockMarketEnv(gym.Env):
-    #metadata = {'render.modes': ['human']}
+    # metadata = {'render.modes': ['human']}
 
     # (sort, long, idle) or (sort, long)
 
@@ -20,22 +21,23 @@ class StockMarketEnv(gym.Env):
     low = 'Low'
     new_stock = 'new_stock'
 
-    #FEATURES
+    # FEATURES
     c_o = 'c_o'
     c_l_h = 'c_l_h'
     o_c_1 = 'o_c_1'
     aggr_mean = 'aggr_mean'
     aggr_std = 'aggr_std'
 
-    #state
+    # state
     HOLD = 'Hold'
     NO_HOLD = 'Not hold'
+
     def __init__(self):
         super(StockMarketEnv, self).__init__()
         self.action_space = spaces.Discrete(self.num_of_action)
-        self.observation_space = spaces.Box(low = -1, high=1,shape=(1,6), dtype=float)
-        self.data_frame = pd.read_csv("traning_data\\traning_features.csv")
-        self.balance = 10000
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(1, 6), dtype=float)
+        #self.data_frame = pd.read_csv("traning_data\\traning_features.csv")
+        self.balance = 1
         self.hold = False
         self.buy_price = 0
         self.iter = 0
@@ -55,58 +57,60 @@ class StockMarketEnv(gym.Env):
 
     def step(self, action):
         reward = 0
+        done = False
         self.iter = self.iter + 1
-        if self.iter == 10000: #2340390
+        if self.iter > 2340390:  # 2340390
             print("Reward:" + str(self.cum_rewarsd))
+            print("DOOONNNNNEE")
             done = True
-            self.iter = 0
         if self.data.new_stock[self.iter] == 0:
             print("New stock")
+            print("Reward:" + str(self.cum_rewarsd))
+            self.balance = 1
+
+            self.cum_rewarsd = 0
             action = 2
 
-        #if action == 0:
+        # if action == 0:
 
         if action == 1:
             if self.state is self.NO_HOLD:
-                #print("BUY")
+                # print("BUY")
                 self.buy_price = self.data.Close[self.iter]
                 self.state = self.HOLD
         if action == 2:
             if self.state is self.HOLD:
-                #print("SELL")
+                # print("SELL")
                 self.state = self.NO_HOLD
                 if math.isclose(self.buy_price, 0):
                     print("Fuck price")
                     print(self.iter)
                     reward = 0
                 else:
-                    reward = (self.data.Open[self.iter] - self.buy_price) / self.buy_price
-                self.cum_rewarsd = self.cum_rewarsd + reward
+                    if self.data.new_stock[self.iter] == 1:
+                        reward = (self.data.Open[self.iter] - self.buy_price) / self.buy_price
+                    else:
+                        reward = (self.data.Open[self.iter - 1] - self.buy_price) / self.buy_price
 
-        done = False
-
-        reward = self.calc_reward()
-
+        obsrew = self.calc_reward()
         obs = np.array([self.data.c_o[self.iter], self.data.c_l_h[self.iter], self.data.o_c_1[self.iter],
-                        self.data.aggr_mean[self.iter], self.data.aggr_std[self.iter], reward], dtype=float)
+                        self.data.aggr_mean[self.iter], self.data.aggr_std[self.iter], obsrew], dtype=float)
         norm_obs = np.linalg.norm(obs)
-        if  norm_obs is 0:
+        if norm_obs is 0:
             norm_obs = 1
             print(obs)
             print(self.iter)
 
-        obs = obs/norm_obs
+        obs = obs / norm_obs
 
         if math.isnan(obs[0]):
             norm_obs = 1
             print(obs)
             print(self.iter)
             obs = np.array([self.data.c_o[self.iter], self.data.c_l_h[self.iter], self.data.o_c_1[self.iter],
-                        self.data.aggr_mean[self.iter], self.data.aggr_std[self.iter], reward], dtype=float)
+                            self.data.aggr_mean[self.iter], self.data.aggr_std[self.iter], obsrew], dtype=float)
         if math.isnan(reward):
             print("Reward is a fucking Nan you scam bitch fuck")
-
-        self.iter = self.iter + 1
         return obs, reward, done, {}
 
     def reset(self):
@@ -115,6 +119,7 @@ class StockMarketEnv(gym.Env):
         self.cum_rewarsd = 0
         self.state = self.NO_HOLD
         reward = 0
+        self.balance = 1
         obs = np.array([self.data.c_o[self.iter], self.data.c_l_h[self.iter], self.data.o_c_1[self.iter],
                         self.data.aggr_mean[self.iter], self.data.aggr_std[self.iter], reward], dtype=float)
         return obs
@@ -124,16 +129,24 @@ class StockMarketEnv(gym.Env):
 
 
 asd = make_vec_env(StockMarketEnv, n_envs=4)
-print(asd)
+# 512
+model = PPO2(MlpLstmPolicy, asd, n_steps=1024, gamma=0.999
+             , verbose=1, tensorboard_log="c:\\tmp\\roxenv_sh\\traning_data\\", full_tensorboard_log=False,
+             policy_kwargs={'n_lstm': 48})
+#model = PPO2.load("c:\\tmp\\roxenv_sh\\traning_data\\trader_lstm_32_env_4_iter_3", env=asd)
 
-model = PPO2(MlpLstmPolicy, asd, n_steps=2048,verbose=2, tensorboard_log="c:\\tmp\\roxenv_sh\\traning_data\\",policy_kwargs={'n_lstm': 32})
-
-model.learn(total_timesteps=(2048 * 4 * 10000)
-            )
+for i in range(1,21):
+    model.learn(total_timesteps=(2340390)) # 2340390 * 50))
+    model.save("c:\\tmp\\roxenv_sh\\traning_data\\trader_balance_norm_reward_lstm_" + str(48) + "_env_" + str(4) + "_iter_" + str(i))
 
 obs = asd.reset()
 
+
 for i in range(10000):
-    action, _state = model.predict(obs)
+    action, _state = model.predict(obs, deterministic=True)
+    print(action)
+    prob = model.action_probability(obs)
+    print(prob)
     obs, reward, dones, info = asd.step(action)
     print(reward)
+
